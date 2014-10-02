1e6d17ce8dd63d4eda6a96423dfdf61168b546b0
martens <martens@172.17.18.117>
2014-10-02 11:47:46 -0400

    Work done on October 3rd

--- src/NewMain.java
+++ src/NewMain.java
@@ -5,6 +5,7 @@ public class NewMain {
   
     public static void main(String[] args) {
         System.out.println("Hey");
+        System.out.println("Nwe thing");
     }
     
 }
---
+++ src/NewMain1.java
@@ -0,0 +1,9 @@
+
+public class NewMain1 {
+
+ 
+    public static void main(String[] args) {
+        //a new file to be added before I commit
+    }
+    
+}
