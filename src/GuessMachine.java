3688cc8deb457283993f44bd0420a6e5d6326b76
John Martens <John.Martens@gmail.com>
2014-10-07 08:42:07 -0400

    Started Guessing Game Oct 6

--- src/GuessMachine.java
+++ src/GuessMachine.java
@@ -8,6 +8,31 @@ public class GuessMachine {
         guess=0;
         numGuesses=0;
     }
+    
+    public boolean setGuess(int g){
+        boolean okguess=(g>0 && g<=100);
+        if (okguess){
+            guess=g;
+            numGuesses++;
+            return true;
+        }
+        else return false;
+    }
+    
+    public String giveHint(){
+        if (guess>number)
+            return(guess + " is too high, guess lower.");
+        else if(guess<number)
+            return(guess + " is too low, guess higher");
+        else
+            return "You got it!";
+    }
+    
+    public int getNumGuesses(){
+        return numGuesses;
+    }
+    
+    
     //to do - add methods:
     //giveHint(), setGuess(), getNumGuesses()
 }
